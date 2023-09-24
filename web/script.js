d4b8467b1e169cd0390101176c8f9fae6ea7b0d9
john lai <john.lai@evermight.com>
2023-09-24 11:55:47 -0400

    simplified ui further

--- web/script.js
+++ web/script.js
@@ -10,6 +10,17 @@ const DISPLAY = {
 };
 document.addEventListener('DOMContentLoaded', _ => {
   document.querySelectorAll('[name=action]').forEach(btn => {
+    // If there are no other buttons pressed, then stop all actions
+    btn.addEventListener('focusout', evt => {
+      if(document.querySelectorAll('[name="action"]:focus').length < 1) {
+        fetch('./process.php?action=ss')
+        .then(res => res.text())
+        .then(res => {
+          document.getElementById('current-action').innerHTML = DISPLAY[res].label;
+          document.getElementById('current-action').className = DISPLAY[res].cls;
+        });
+      }
+    });
     btn.addEventListener('click', evt => 
       fetch('./process.php?action='+evt.currentTarget.value)
       .then(res => res.text())
