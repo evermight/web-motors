bd419a2848dee0062dfc477d5e481c2aeac5a294
john.lai@evermight.com <john.lai@evermight.com>
2023-10-03 07:50:35 -0400

    esp8266 pins, but not tested

---
+++ esp32/motors/config.esp8266.sh
@@ -0,0 +1,21 @@
+#define SSID_NAMES {"ssid-primary", "ssid-backup"}
+#define SSID_PASSES {"pass-primary", "pass-backup"}
+#define SSID_LEN 2
+#define API_URL "http://url/motion.txt"
+
+#define LOOP_SPEED 20
+
+#define PWM_FREQ 30000
+#define PWM_RESOLUTION 8
+#define PWM_DUTY_CYCLE 255
+#define PWM_DUTY_CYCLE_SLOW 210
+
+#define MOTOR1_PIN1 14
+#define MOTOR1_PIN2 13
+#define ENABLE1_PIN 5
+#define PWM_CHANNEL_1 0
+
+#define MOTOR2_PIN1 2
+#define MOTOR2_PIN2 0
+#define ENABLE2_PIN 4
+#define PWM_CHANNEL_2 1
