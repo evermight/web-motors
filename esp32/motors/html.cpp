d6b5c575e2216a40496b1089d792fe62def7adc0
john lai <john.lai@evermight.com>
2023-10-07 10:39:53 -0400

    mqtt web connection

--- esp32/motors/html.cpp
+++ esp32/motors/html.cpp
@@ -47,21 +47,6 @@ function send() {
   printCurrentSettings();
 }
 
-function getCookie(cname) {
-  let name = cname + '=';
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return '';
-}
 document.addEventListener('DOMContentLoaded',()=>{
   preloadForm();
   printCurrentSettings();
--- web/script.js
+++ web/script.js
@@ -8,6 +8,58 @@ const DISPLAY = {
    "ss":{"cls":"", "label":"&#9632;"},
    "fb":{"cls":"invert", "label":"&#9100;"}
 };
+var MQTT;
+const fields = 'mqtt_server,mqtt_port,mqtt_topic,mqtt_user,mqtt_pass'.split(',');
+
+const loadSettingsFromLocalStorage = () => {
+  fields.forEach(item=>document.getElementById(item).value = localStorage.getItem(item));
+}
+
+const loadSettingsFromUrl = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  fields.forEach(field => {
+    const param = urlParams.get(field);
+    if(param) {
+      document.getElementById(field).value = param;
+    }
+  });
+}
+const MQTTConnectSuccess = () => {
+  document.querySelector('.direction-pad').classList.add('enable');
+  document.querySelector('.settings').classList.remove('enable');
+
+  fields
+    .forEach(item=>localStorage.setItem(item,document.getElementById(item).value));
+};
+const MQTTConnect = () => {
+  const host = document.getElementById("mqtt_server").value;
+  const port = parseInt(document.getElementById("mqtt_port").value);
+  const username = document.getElementById("mqtt_user").value;
+  const password = document.getElementById("mqtt_pass").value;
+  const cname="robo-"+Math.floor(Math.random() * 10000);
+
+  MQTT = new Paho.MQTT.Client(host,port,cname);
+  const options = {
+    timeout: 3,
+    onSuccess: MQTTConnectSuccess,
+    userName:username,
+    password,
+  };
+  MQTT.connect(options);
+};
+const btnPress = value => {
+	const topic = document.getElementById("mqtt_topic").value;
+	message = new Paho.MQTT.Message(value);
+	message.destinationName = topic;
+	MQTT.send(message);
+}
+/*
+const btnPress = value => {
+  fetch('./process.php?action='+value)
+  .then(res => res.text())
+  .then(res => setBtn(res))
+}
+*/
 const setBtn = val => {
   document.querySelectorAll('[name=action]').forEach(btn=>btn.classList.remove('active'));
   const btn = document.querySelector('[name=action][value='+val+']').classList.add('active');
@@ -31,10 +83,8 @@ document.addEventListener('DOMContentLoaded', _ => {
         .then(res => setBtn(res));
       }
     });
-    btn.addEventListener('click', evt => 
-      fetch('./process.php?action='+evt.currentTarget.value)
-      .then(res => res.text())
-      .then(res => setBtn(res))
-    );
+    btn.addEventListener('click', evt => btnPress(evt.currentTarget.value));
   });
+  loadSettingsFromLocalStorage();
+  loadSettingsFromUrl();
 });
